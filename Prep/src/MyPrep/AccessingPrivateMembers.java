45e0e976fad87c9529c4e505c9548218db16a27c
Swati Pal <swatipal39@gmail.com>
2024-02-02 23:01:32 +0530

    Java program to access the private members of a class using the public methods

---
+++ Prep/src/MyPrep/AccessingPrivateMembers.java
@@ -0,0 +1,36 @@
+package MyPrep;
+
+public class Farm{
+	private double length;
+	private double width;
+	
+	public void setSides(double l, double w) {
+		length = l;
+		width = w;
+	}
+	
+	public double area() {
+		return length*width;
+	}
+	
+	public double getLength() {
+		return length;
+	}
+	public double getWidth() {
+		return width;
+	}
+}
+public class AccessingPrivateMembers {
+
+	public static void main(String[] args) {
+		Farm farm1 = new Farm();
+		farm1.setSides(40.0,20.0);
+		double farmArea = farm1.area();
+
+		System.out.println("Area of the farm is:"+farmArea);
+		System.out.println("Length of the farm is: "+farm1.getLength());
+		System.out.println("Width of the farm is: "+farm1.getWidth());
+
+	}
+
+}

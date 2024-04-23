2a827037f2ca27f368acba7957ddddbec6784312
Swati Pal <swatipal39@gmail.com>
2024-04-23 21:14:16 +0530

    Selenium automation of for page using the Page objects for different functionality

--- SeleniumLinkedIn/src/main/java/AutomationSe/ConfirmationPage.java
+++ SeleniumLinkedIn/src/main/java/AutomationSe/ConfirmationPage.java
@@ -1,6 +1,6 @@
 package AutomationSe;
 
-public class PageObjects {
+public class ConfirmationPage {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
--- SeleniumLinkedIn/src/main/java/AutomationSe/Workflow.java
+++ SeleniumLinkedIn/src/main/java/AutomationSe/Workflow.java
@@ -21,41 +21,18 @@ public class Workflow {
 		
 		driver.get("https://formy-project.herokuapp.com/form");
 		
-		submitForm(driver);
+		FormPage formpage = new FormPage();
+		formpage.submitForm(driver);
 		
-		waitForAlertBanner(driver);
+		ConfirmationPage confirmpage = new ConfirmationPage();
+		confirmpage.waitForAlertBanner(driver);
 		
 		
-		assertEquals("The form was successfully submitted!", getAlertBannerText(driver));
+		assertEquals("The form was successfully submitted!", confirmpage.getAlertBannerText(driver));
 		
 		driver.quit();
 
 	}
-	
-	public static void submitForm(WebDriver driver) {
-		driver.findElement(By.id("first-name")).sendKeys("John");
-		driver.findElement(By.id("last-name")).sendKeys("Doe");
-		
-		driver.findElement(By.id("job-title")).sendKeys("QA Engineer");
-		
-		driver.findElement(By.id("radio-button-2")).click();
-		
-		driver.findElement(By.id("checkbox-2")).click();
-		
-		driver.findElement(By.cssSelector("option[value='1']")).click();
-		
-		driver.findElement(By.id("datepicker")).sendKeys("05/20/2023");
-		driver.findElement(By.id("datepicker")).sendKeys(Keys.RETURN);			//To close the datepicker
-		
-		driver.findElement(By.cssSelector(".btn.btn-lg.btn-primary")).click();
-	}
 
-	public static void waitForAlertBanner(WebDriver driver) {
-		WebDriverWait wait = new WebDriverWait(driver,10);
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("alert"))); //Wait for 10seconds until alert class appears
-	}
 	
-	public static String getAlertBannerText(WebDriver driver) {
-		return driver.findElement(By.className("alert")).getText();
-	}
 }

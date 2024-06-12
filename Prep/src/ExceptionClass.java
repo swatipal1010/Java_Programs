70c347d9eca1b7e6b57df002b35ddc9b66bdbfe5
Swati Pal <swatipal39@gmail.com>
2024-06-12 19:36:06 +0530

    Exception class along with try-class blocks

---
+++ Prep/src/ExceptionClass.java
@@ -0,0 +1,17 @@
+
+class MyExceptions extends Exception{
+	@Override
+	public String toString() {
+		return super.toString()+ "I am in toString() method";
+	}
+}
+
+
+public class ExceptionClass {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}

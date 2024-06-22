431b2b8a6b00bb3321ee46c85ae0f17f92734666
Swati Pal <swatipal39@gmail.com>
2024-06-22 23:59:51 +0530

    Finding union of 2 arrays using hashset and through traversing the arrays

--- DSA/src/Hashing/UnionOf2Arrays.java
+++ DSA/src/Hashing/UnionOf2Arrays.java
@@ -2,6 +2,8 @@ package Hashing;
 import java.util.*;
 
 public class UnionOf2Arrays {
+	
+	//method-1 Using HashSet
 	public static int doUnion(int a[], int b[]) {
 		int l1 = a.length;
 		int l2 = b.length;
@@ -18,12 +20,45 @@ public class UnionOf2Arrays {
 		return set.size();
 	}
 	
+	
+	//Method-2 Iterating through the arrays
+	public static int[] doUnion2(int[] nums1, int[] nums2) {
+		List<Integer> resultList = new ArrayList<>();
+	    
+	    for (int num : nums1) {
+	        if (!resultList.contains(num)) {
+	            resultList.add(num);
+	        }
+	    }
+	    
+	    for (int num : nums2) {
+	        if (!resultList.contains(num)) {
+	            resultList.add(num);
+	        }
+	    }
+	    
+	    int[] result = new int[resultList.size()];
+	    for (int i = 0; i < resultList.size(); i++) {
+	        result[i] = resultList.get(i);
+	    }
+	    
+	    return result;
+	}
+	
+	
 	public static void main(String[] args) {
 		int[] a = {1,1,2,3,4,2};
 		int b[] = {3,5,2,2,9};
 		
+		System.out.println("Union of the elements is: ");
+		int[] arr = doUnion2(a,b);
+		for(int val: arr) {
+			System.out.print(val+" ");
+		}
+		System.out.println();
 		int count = doUnion(a,b);
 		System.out.println("Union of array a and b contains: "+count+" elments.");		//1 2 3 4 5 9
+		
 
 	}
 

ffd8d0187704bcb4c9be779a9343d88061cc57cf
Swati Pal <swatipal39@gmail.com>
2024-08-16 11:03:15 +0530

    Added comments and modified code to take user input

--- DSA/src/Stack/StackUsingArrayList.java
+++ DSA/src/Stack/StackUsingArrayList.java
@@ -1,6 +1,7 @@
 package Stack;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class StackUsingArrayList {
 
@@ -20,11 +21,11 @@ public class StackUsingArrayList {
         // pop operation
         public static int pop() {
             if (isEmpty()) {
-                return -1;
+                return -1;		//If the arraylist(i.e. stack) is empty, we have nothing to pop out
             }
 
-            int top = list.get(list.size() - 1);
-            list.remove(list.size() - 1); // Corrected to remove by index
+            int top = list.get(list.size() - 1);		//top element is the last element of the arrayList
+            list.remove(list.size() - 1); 				// Corrected to remove by index
             return top;
         }
 
@@ -39,11 +40,23 @@ public class StackUsingArrayList {
 
     public static void main(String[] args) {
         Stack s = new Stack();
-        s.push(1);
-        s.push(2);
-        s.push(3);
-        s.push(4);
-
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of elements you want to store in stack: ");
+        int size = sc.nextInt();
+        if(size!=0) {
+        	System.out.println("Enter "+size+" elements into the stack: ");
+        	while(size!=0) {
+        		int val = sc.nextInt();
+        		s.push(val);
+        		size--;
+        	}
+        }
+//        s.push(1);
+//        s.push(2);
+//        s.push(3);
+//        s.push(4);
+        
+        System.out.println("Elements in the stack are: ");
         while (!s.isEmpty()) {
             System.out.println(s.pop());
         }

439d32ce33604eb2a419dba5b9e5dee7f6a34043
Swati Pal <swatipal39@gmail.com>
2024-08-30 12:53:07 +0530

    Parameterized and functionalrecursive programs to add natural numbers from 1 to N and calculate factorial of a number

--- DSA/src/Recursion/Sum1ToN.java
+++ DSA/src/Recursion/Sum1ToN.java
@@ -3,11 +3,40 @@ import java.util.Scanner;
 
 public class Sum1ToN {
 	
+	//Parameterized Recursion
+	public static void sumParam(int i, int sum) {
+		if(i<1) {
+			System.out.println(sum);
+			return;
+		}
+		sumParam(i-1, sum+i);
+	}
+	
+	//Functional Recursion
 	public static int sum(int n) {
 		if(n==1) {
 			return 1;
 		}
 		return n+sum(n-1);
+	}
+	
+	
+	//Parameterized factorial method
+	public static void factorialP(int n, int fact) {
+		if(n==1) {
+			System.out.println(fact);
+			return;
+		}
+		factorialP(n-1, fact*n);
+	}
+	
+	
+	//Functional Factorial method
+	public static int factorialF(int n) {
+		if(n==1) {
+			return 1;
+		}
+		return n*factorialF(n-1);
 	}
 
 	public static void main(String[] args) {
@@ -16,6 +45,14 @@ public class Sum1ToN {
 		int number = sc.nextInt();
 		
 		System.out.println("Sum of numbers from 1 to "+number+" is: "+sum(number));
+		System.out.print("Sum of numbers from 1 to "+number+" is: ");
+		sumParam(number, 0);
+		System.out.println(" ");
+		
+		System.out.print("Factorial of "+number+" is: ");
+		factorialP(number, 1);
+		
+		System.out.print("Factorial of "+number+" is: "+factorialF(number));
 
 	}
 

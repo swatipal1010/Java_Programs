34f3028abf0e275326446f2e1c6035332ef36335
Swati Pal <swatipal39@gmail.com>
2024-11-23 13:05:32 +0530

    Program to find the target element in the sorted matrix in time complexity = O(m*logn) where m = no.of rows and n = no. of columns

--- DSA/src/Arrays/TargetInSortedMatrix.java
+++ DSA/src/Arrays/TargetInSortedMatrix.java
@@ -5,6 +5,8 @@ import java.util.Arrays;
 
 public class TargetInSortedMatrix {
 	
+	
+	//METHOD-1 Time Complexity = O(m+n) where m is the no. of rows and n is the no. of columns
 	public static int[] findTarget(int[][] matrix, int target) {
 		int r = 0;
 		int col = matrix[0].length-1;
@@ -20,6 +22,27 @@ public class TargetInSortedMatrix {
 		}
 		return new int[] {-1,-1};
 	}
+	
+	
+	//METHOD-2 Time Complexity=O(m*logn). Applying Binary Search on each row of the matrix to find the element
+	public static int[] findTarget2(int[][] matrix, int target) {
+	    for (int i = 0; i < matrix.length; i++) {
+	        int start = 0; 
+	        int end = matrix[0].length - 1; 
+	        while (start <= end) { 
+	            int mid = start + (end - start) / 2;
+	            if (matrix[i][mid] == target) {
+	                return new int[] { i, mid };
+	            } else if (matrix[i][mid] < target) {
+	                start = mid + 1;
+	            } else {
+	                end = mid - 1;
+	            }
+	        }
+	    }
+	    return new int[] { -1, -1 }; 
+	}
+
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
@@ -44,7 +67,7 @@ public class TargetInSortedMatrix {
 		System.out.println("Enter the target element from the matrix: ");
 		int target = sc.nextInt();
 		
-		int[] index = findTarget(matrix, target);
+		int[] index = findTarget2(matrix, target);
 		
 		System.out.println("Target element "+target+" liest at index: "+Arrays.toString(index));
 		

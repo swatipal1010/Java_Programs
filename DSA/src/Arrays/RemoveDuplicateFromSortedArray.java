95edde8e0ab088ffe3c7fc3322ab5b6d0b663f3d
Swati Pal <swatipal39@gmail.com>
2024-11-25 16:16:33 +0530

    Corrected the condition inside the for loop

--- DSA/src/Arrays/RemoveDuplicateFromSortedArray.java
+++ DSA/src/Arrays/RemoveDuplicateFromSortedArray.java
@@ -7,7 +7,7 @@ public class RemoveDuplicateFromSortedArray {
 	public static int removeDuplicate(int nums[]) {
 		int k =0;			//k is the pointer that points to the last unique element of the array
 		for(int i=0; i<nums.length-1; i++) {
-			if(nums[i]==nums[i+1]) {
+			if(nums[i]!=nums[i+1]) {
 				nums[k+1]=nums[i+1];
 				k++;
 			}

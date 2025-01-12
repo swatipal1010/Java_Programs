6fdf963f6babaccc3f7067937e6ea8779248845b
Swati Pal <swatipal39@gmail.com>
2025-01-12 16:28:43 +0530

    Program to find the missing numbers from an array having elements in the range 1 to N

--- DSA/src/Sorting/FindMissingNums.java
+++ DSA/src/Sorting/FindMissingNums.java
@@ -4,6 +4,8 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class FindMissingNums {
+	
+	//METHOD:1
     // Method to swap two elements in the array
     public static void swap(int[] arr, int first, int second) {
         int temp = arr[first];
@@ -25,8 +27,7 @@ public class FindMissingNums {
             } else {
                 i++;
             }
-        }
-        
+        }   
         // Find all indices where the value is not correct
         for (int j = 0; j < n; j++) {
             if (arr[j] != j + 1) {
@@ -34,6 +35,24 @@ public class FindMissingNums {
             }
         }
         return missNums;
+    }
+    
+    
+    //METHOD:2
+    public static ArrayList<Integer> findDisappearedNumbers(int[] nums) {
+        boolean[] arr = new boolean[nums.length + 1];
+
+        for (int num : nums) {
+            arr[num] = true; // Mark the number as present
+        }
+
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 1; i <= nums.length; i++) {
+            if (!arr[i]) {
+                list.add(i); // Add missing numbers to the list
+            }
+        }
+        return list;
     }
 
     public static void main(String[] args) {
@@ -49,6 +68,9 @@ public class FindMissingNums {
         
         System.out.println("Array is: " + Arrays.toString(arr));
         ArrayList<Integer> ans = allMissingNums(arr);
+        System.out.println("Missing numbers in the array are: " + ans);
+        
+        ans = findDisappearedNumbers(arr);
         System.out.println("Missing numbers in the array are: " + ans);
         
         sc.close();

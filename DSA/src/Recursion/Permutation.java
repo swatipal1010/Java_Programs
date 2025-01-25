b65a38a187a781eb66233b519b8e7097f8524f49
Swati Pal <swatipal39@gmail.com>
2025-01-25 18:07:24 +0530

    Method added to return the list of list of permutation strings

--- DSA/src/Recursion/Permutation.java
+++ DSA/src/Recursion/Permutation.java
@@ -1,9 +1,11 @@
 package Recursion;
-
+import java.util.List;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Permutation {
 	
+	//Printing permutations
 	public static void permutation(String processed, String unprocessed) {
 		if(unprocessed.isEmpty()) {
 			System.out.println(processed+" ");
@@ -17,6 +19,25 @@ public class Permutation {
 		}
 		
 	}
+	
+	
+	//Returning the list of permutations
+	public static List<String> permutationList(String processed, String unprocessed) {
+        List<String> result = new ArrayList<>();		//list to store all permutations
+        if (unprocessed.isEmpty()) {
+            result.add(processed);
+            return result;
+        }
+        char ch = unprocessed.charAt(0);
+
+        // Loop to insert the character 'ch' into every possible position in the processed string
+        for (int i = 0; i <= processed.length(); i++) {
+            String first = processed.substring(0, i);   // Part before position i
+            String second = processed.substring(i);     // Part after position i
+            result.addAll(permutationList(first + ch + second, unprocessed.substring(1)));
+        }
+        return result;
+    }
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
@@ -25,6 +46,10 @@ public class Permutation {
 		
 		System.out.println("Possible permutations of "+str+" are: ");
 		permutation("", str);
+		
+		List<String> list = permutationList("",str);
+		System.out.println("Permutations of "+str+" are: "+list);
+		
 		
 		sc.close();
 

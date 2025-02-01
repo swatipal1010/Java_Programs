34ac40d5306d693c199f163088310d2653b2918b
Swati Pal <swatipal39@gmail.com>
2025-02-01 19:27:09 +0530

    Removed list as parameter as it is not involved in counting the number of subsets with the sum of digits equal to the given sum

--- DSA/src/Recursion/CountSubsetsWithSumK.java
+++ DSA/src/Recursion/CountSubsetsWithSumK.java
@@ -1,20 +1,16 @@
 package Recursion;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class CountSubsetsWithSumK {
-	public static int subsequence(int arr[],List<Integer> list,int index, int sum,int currentSum) {
+	public static int subsequence(int arr[],int index, int sum,int currentSum) {
 		if(index==arr.length) {
 			return (currentSum == sum) ? 1 : 0;
 		}
 		//Pick array element
-		list.add(arr[index]);
-		int count1 = subsequence(arr,list,index+1,sum,currentSum+arr[index]); 	
+		int count1 = subsequence(arr,index+1,sum,currentSum+arr[index]); 	
 		
 		//Don't pick array element
-		list.remove(list.size() - 1);
-		int count2 = subsequence(arr,list,index+1,sum,currentSum);
+		int count2 = subsequence(arr,index+1,sum,currentSum);
 		
 		return count1+count2;
 	}
@@ -33,10 +29,10 @@ public class CountSubsetsWithSumK {
         System.out.println("Enter the value of sum that should be equal to the sum of digits in subset: ");
         int sum = sc.nextInt();
         
-        List<Integer> list = new ArrayList<>();
-        int ansCount = subsequence(arr, list, 0, sum, 0);
+        int ansCount = subsequence(arr, 0, sum, 0);
         System.out.println("Number of subsets whose sum of digits is equal to the given sum: "+ansCount);
-
+        
+        sc.close();
 	}
 
 }

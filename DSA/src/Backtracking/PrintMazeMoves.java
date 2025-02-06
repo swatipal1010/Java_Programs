bdd099ff95cc0934b742e574cf04aa6107cb76c5
Swati Pal <swatipal39@gmail.com>
2025-02-06 17:14:08 +0530

    Added method to return the ArrayList containing all possible paths to move from first cell to last cell of the maze

--- DSA/src/Backtracking/PrintMazeMoves.java
+++ DSA/src/Backtracking/PrintMazeMoves.java
@@ -1,5 +1,5 @@
 package Backtracking;
-
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class PrintMazeMoves {
@@ -18,6 +18,22 @@ public class PrintMazeMoves {
 		
 	}
 	
+	public static ArrayList<String> pathReturn(String processed, int row, int col){
+		if(row==1 && col==1) {
+			ArrayList<String> list = new ArrayList<>();
+			list.add(processed);
+			return list;
+		}
+		ArrayList<String> list = new ArrayList<>();
+		if(row>1) {
+			list.addAll(pathReturn(processed+"D", row-1, col));
+		}
+		if(col>1) {
+			list.addAll(pathReturn(processed+"R", row, col-1));
+		}
+		return list;
+	}
+	
 	public static void main(String[] args) {
 		Scanner sc  = new Scanner(System.in);
 		System.out.println("Enter the number of rows and cols maze will have: ");
@@ -25,6 +41,9 @@ public class PrintMazeMoves {
 		
 		System.out.println("Ways to travel the maze from first cell to the last cell are: ");
 		printWays("",size,size);
+		
+		ArrayList<String> ansList = pathReturn("",size,size);
+		System.out.println("All possible paths are: "+ansList);
 		
 		sc.close();
 	}
